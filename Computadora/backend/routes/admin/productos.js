27936378c6966cfbdfb98c1199b7a2978f431b82
Oscar Cogno <cognooscaralberto25@gmail.com>
2022-08-15 12:16:31 -0300

    CRUD (parte 2)

--- Computadora/backend/routes/admin/productos.js
+++ Computadora/backend/routes/admin/productos.js
@@ -1,52 +1,64 @@
 var express = require('express');
 var router = express.Router();
 var productosModel = require('../../models/productosModel');
-
+const util = require('util');
+const cloudinary = require('cloudinary').v2;
+const uploader = util.promisify(cloudinary.uploader.upload);
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
 
   var productos = await productosModel.getProductos();
 
-  res.render('admin/productos',{
-    layout:'admin/layout',
-     persona: req.session.nombre,
-     productos
-  } );
+  res.render('admin/productos', {
+    layout: 'admin/layout',
+    persona: req.session.nombre,
+    productos
+  });
 });
 
 router.get('/agregar', (req, res, next) => {
-   res.render('admin/agregar',{
-     layout: 'admin/layout'
-   })
+  res.render('admin/agregar', {
+    layout: 'admin/layout'
+  })
 });
 
 router.post('/agregar', async (req, res, next) => {
-   try {
-        if (req.body.titulo != "" && req.body.descripcion != "" && req.body.precio != "") {
-          await productosModel.insertProducto(req.body);
-          res.redirect('/admin/productos')
-        } else {
-            res.render('admin/agregar', {
-                 layout: 'admin/layout',
-                 error: true,
-                 message: 'Todos los campos son requeridos'
-            })
-        }
-   } catch (error) {
-     console.log(error)
-     res.render('admin/agregar', {
-         layout: 'admin/layout',
-         error: true,
-         message: 'No se cargo el producto'
-     })
-   }
+  try {
+
+    var imagen = '';
+    if (req.files && Object.keys(req.files).length > 0) {
+      imagen = req.files.imagen;
+      imagen = (await uploader(imagen.tempFilePath)).public_id;
+    }
+
+    if (req.body.titulo != "" && req.body.descripcion != "" && req.body.precio != "") {
+      await productosModel.insertProducto({
+        req.body,
+        imagen
+      });
+      res.redirect('/admin/productos')
+    } else {
+      res.render('admin/agregar', {
+        layout: 'admin/layout',
+        error: true,
+        message: 'Todos los campos son requeridos'
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    res.render('admin/agregar', {
+      layout: 'admin/layout',
+      error: true,
+      message: 'No se cargo el producto'
+    })
+  }
 })
 
 router.get('/eliminar/:id', async (req, res, next) => {
-   var id = req.params.id;
-   await productosModel.deleteProductosById(id);
-   res.redirect('/admin/productos');
+  var id = req.params.id;
+  await productosModel.deleteProductosById(id);
+  res.redirect('/admin/productos');
 });
 
 
@@ -54,7 +66,7 @@ router.get('/editar/:id', async (req, res, next) => {
   var id = req.params.id;
   var producto = await productosModel.getProductoById(id);
 
-  
+
   res.render('admin/editar', {
     layout: 'admin/layout',
     producto
@@ -66,23 +78,23 @@ router.post('/editar', async (req, res, next) => {
   try {
 
     var obj = {
-                imagen: req.body.imagen,
-                producto: req.body.producto,
-                precio: req.body.precio,
-                descuento: req.body.descuento
+      imagen: req.body.imagen,
+      producto: req.body.producto,
+      precio: req.body.precio,
+      descuento: req.body.descuento
     }
-         console.log(obj)
+    console.log(obj)
 
-         await productosModel.editarProductoById(obj, req.body.id);
-         res.redirect('/admin/productos');
+    await productosModel.editarProductoById(obj, req.body.id);
+    res.redirect('/admin/productos');
 
   } catch (error) {
-      console.log(error)
-      res.render('admin/editar', {
-          layout: 'admin/layout',
-          error: true,
-          message: 'No se modifico el producto'
-      })
+    console.log(error)
+    res.render('admin/editar', {
+      layout: 'admin/layout',
+      error: true,
+      message: 'No se modifico el producto'
+    })
   }
 })
 
